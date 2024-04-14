cb2ca11e214d7160cef1b4f54835a504bbef6f43
tobiashh19 <122474823+tobiashh19@users.noreply.github.com>
2024-04-14 16:33:09 -0600

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = """
 bot_template = """
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/RDR6q2g/medical-robot.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNVi9cbmMkUabLiF_3kfI94qngwPIM4gnrztEUv6Hopw&s" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
